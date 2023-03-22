9a0423b4bb6f07b8d2babc2f57f4e570a4ebf2c7
aw5862 <aw5862@email.vccs.edu>
2023-03-21 20:54:49 -0400

    Added python_basics assignment

---
+++ python_basics.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+
+name = input("Enter name: ")
+age = input("Enter age: ")
+degree = input("Enter degree program: ")
+
+remaining_age = int(age) * 3 / 2
+
+print(name + "\n" + str(remaining_age) + "\n" + degree)
